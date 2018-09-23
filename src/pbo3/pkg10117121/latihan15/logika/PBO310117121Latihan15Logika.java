923319227bed91c69461a0ec58c614aad01a5205
CHEEKIBREEKIIVDAMKE <CHEEKIBREEKIIVDAMKE@DESKTOP-S13CU08>
2018-09-23 19:00:42 +0700

    rho-ink

---
+++ src/pbo3/pkg10117121/latihan15/logika/PBO310117121Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117121.latihan15.logika;
+
+/**
+ *
+ * @author CHEEKIBREEKIIVDAMKE
+ *NAMA                 : M Rifqi S M
+ *KELAS                : PBO-3 
+ *NIM                  : 10117121
+ *Program Deskripsi    : Program ini berisi program menampilkan hasil fungsi logika
+ */
+public class PBO310117121Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + (!a));
+        System.out.println("!(a && b) = " + (!(a && b)));
+
+    }
+    
+}
